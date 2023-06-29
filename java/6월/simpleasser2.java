5693e417bf72a036dbd6924d14d4c836de6014b1
hye-min-jo <johm0309@gmail.com>
2023-06-29 12:45:54 +0900

    변수,상수 공부

---
+++ java/6월/simpleasser2.java
@@ -0,0 +1,10 @@
+class SimpleAdder2 {
+    public static void main(String args[]){
+        int num;
+        num=10+20;
+        if (num>10)
+           System.out.println("계산결과가 10보다 큽니다.");
+
+    }
+    
+}
