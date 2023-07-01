c4d52f37b70893439c95ed3dde271f7183937256
hye-min-jo <johm0309@gmail.com>
2023-07-01 23:56:25 +0900

    산술연산자.md

---
+++ java/7월/cal.java
@@ -0,0 +1,11 @@
+
+class Calculation {
+    public static void main(String[] args){
+        int num1=5;
+        int num2=2;
+
+        int result1=num1+num2;
+        System.out.println(result1);
+    }
+    
+}
