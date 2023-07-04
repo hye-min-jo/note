75687b57d076980b464e12a57afb39c2c53a2891
hye-min-jo <johm0309@gmail.com>
2023-07-04 13:09:22 +0900

    cal.java

---
+++ java/7월/repeat.java
@@ -0,0 +1,11 @@
+class repeat {
+    public static void main(String args[]){
+        int num=0;
+        while (num<10){
+            System.out.println("Hello, Java");
+            num=num+1;
+
+        }
+    }
+    
+}
