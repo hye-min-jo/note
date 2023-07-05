9787a14ee9739ad53aad020080a50bdb2d7b89d2
hye-min-jo <johm0309@gmail.com>
2023-07-05 21:10:23 +0900

    스위치

---
+++ java/7월/switchmon.java
@@ -0,0 +1,16 @@
+class switchmon {
+    public static void main(String[] args){
+        int num=1;
+        switch(num){
+            case 1 :
+              System.out.println("1입니다.");
+              break;
+            case 2:
+              System.out.println("2입니다.");
+            case 3 :
+              System.out.println("3입니다.");  
+                
+        }
+    }
+    
+}
